9091fc6d803590dc22ebd4747e886ffc95ef73c7
Vladimir Vranceanu <v.vranceanu@unifun.com>
2020-12-07 10:05:24 +0200

    String class implementation

---
+++ MyString.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace MyStringApp
+{
+	class MyString
+	{
+
+		private readonly char[] _charset;
+
+		private readonly int _length = 0;
+
+		private int _hashCode = -1;
+
+		public static readonly MyString EMPTY_STRING;
+
+		static MyString()
+		{
+			EMPTY_STRING = new MyString();
+		}
+
+		public MyString(char[] charset)
+		{
+			_charset = charset;
+			_length = charset.Length;
+		}
+
+		public MyString(string str) : this(str.ToCharArray())
+		{ }
+		public MyString(MyString str) : this(str.ToCharArray())
+		{ }
+		private MyString() : this(new char[0])
+		{ }
+		public MyString(char c, int count)
+		{
+			if (count < 0)
+				throw new ArgumentOutOfRangeException();
+
+			_charset = new char[count];
+			_length = count;
+			for (int i = 0; i < count; i++)
+				_charset[i] = c;
+		}
+
+		public MyString(char[] charset, int startIndex, int length)
+		{
+			if (charset == null)
+				throw new ArgumentNullException();
+
+			if (startIndex < 0 || length < 0 || startIndex + length > charset.Length)
+				throw new ArgumentOutOfRangeException();
+
+
+			_charset = new char[length];
+			Array.Copy(charset, startIndex, _charset, 0, length);
+		}
+
+		public MyString ToUpper()
+		{
+			char[] uppercaseArray = new char[_length];
+			for (int i = 0; i < _length; i++)
+				uppercaseArray[i] = Char.ToUpper(_charset[i]);
+			return new MyString(uppercaseArray);
+		}
+
+		public MyString ToLower()
+		{
+			char[] lowercaseArray = new char[_length];
+			for (int i = 0; i < _length; i++)
+				lowercaseArray[i] = Char.ToLower(_charset[i]);
+			return new MyString(lowercaseArray);
+		}
+
+		public bool Contains(MyString str)
+		{
+			return Contains(str.ToCharArray());
+		}
+
+		public bool Contains(char[] charset)
+		{
+			bool contains = false;
+			for (int i = 0; i < _length; i++)
+			{
+				if (this[i] == charset[0])
+				{
+					for (int j = 0; j < charset.Length; j++)
+					{
+						if (i + j > _length - 1)
+							break;
+
+						if (this[i + j] != charset[j])
+						{
+							break;
+						}
+						else if (j == charset.Length - 1)
+						{
+							contains = true;
+						}
+					}
+				}
+			}
+			return contains;
+		}
+
+		public bool StartsWith(MyString str)
+		{
+			return StartsWith(str.ToCharArray());
+		}
+
+		public bool StartsWith(char[] charset)
+		{
+			for (int i = 0; i < charset.Length; i++)
+			{
+				if (i > _length - 1)
+					return false;
+
+				if (_charset[i] != charset[i])
+					return false;
+			}
+
+			return true;
+		}
+
+		public bool EndsWith(MyString str)
+		{
+			return EndsWith(str.ToCharArray());
+		}
+
+		public bool EndsWith(char[] charset)
+		{
+			if (charset.Length > _length)
+				return false;
+
+			for (int i = 0; i < charset.Length; i++)
+			{
+
+				if (_charset[(_length - charset.Length) + i] != charset[i])
+					return false;
+			}
+
+			return true;
+		}
+
+		public MyString TrimStart()
+		{
+			int index = 0;
+			bool next = true;
+			while (next)
+			{
+				if (index == _length - 1)
+					break;
+
+				if (_charset[index] == ' ')
+					index++;
+				else
+					next = false;
+			}
+
+			char[] trimmed = new char[_length - index];
+			Array.Copy(_charset, index, trimmed, 0, _length - index);
+			return new MyString(trimmed);
+		}
+
+		public MyString TrimEnd()
+		{
+			int index = _length - 1;
+			bool next = true;
+			while (next)
+			{
+				if (index == 0)
+					break;
+
+				if (_charset[index] == ' ')
+					index--;
+				else
+					next = false;
+			}
+
+			char[] trimmed = new char[index + 1];
+			Array.Copy(_charset, 0, trimmed, 0, index + 1);
+			return new MyString(trimmed);
+		}
+
+		public MyString Trim()
+		{
+			return TrimStart().TrimEnd();
+		}
+
+		public static MyString operator +(MyString str1, MyString str2)
+		{
+			char[] array = new char[str1.Lenght + str2.Lenght];
+			Array.Copy(str1.ToCharArray(), 0, array, 0, str1.Lenght);
+			Array.Copy(str2.ToCharArray(), 0, array, str1.Lenght, str2.Lenght);
+			return new MyString(array);
+		}
+
+		public static bool operator ==(MyString str1, MyString str2)
+		{
+			if (str1.GetHashCode() != str2.GetHashCode())
+				return false;
+
+			if (str1.Lenght != str2.Lenght)
+				return false;
+
+			for (int i = 0; i < str1.Lenght; i++)
+			{
+				if (str1[i] != str2[i])
+					return false;
+			}
+
+			return true;
+		}
+
+		public static bool operator !=(MyString str1, MyString str2)
+		{
+			if (str1.GetHashCode() != str2.GetHashCode())
+				return true;
+
+			if (str1.Lenght != str2.Lenght)
+				return true;
+
+			for (int i = 0; i < str1.Lenght; i++)
+			{
+				if (str1[i] != str2[i])
+					return true;
+			}
+
+			return false;
+		}
+
+		public static bool operator >(MyString str1, MyString str2)
+		{
+			return str1.Lenght > str2.Lenght;
+		}
+
+		public static bool operator <(MyString str1, MyString str2)
+		{
+			return str1.Lenght < str2.Lenght;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj.GetType() == this.GetType())
+			{
+				return (this == (MyString)obj);
+			}
+
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			if (_hashCode != -1)
+				return _hashCode;
+
+			int hash = 0;
+			for (int i = 0; i < _length; i++)
+				hash += _charset[i] * 31 ^ (_length - 1);
+
+			_hashCode = hash;
+
+			return _hashCode;
+		}
+
+
+		public char this[int index]
+		{
+			get
+			{
+				if (index < 0 || index > _length)
+					throw new IndexOutOfRangeException();
+
+				return _charset[index];
+			}
+		}
+
+		public int Lenght { get => _length; }
+
+		public char[] ToCharArray()
+		{
+			return _charset;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			foreach (char c in _charset)
+				sb.Append(c);
+
+			return sb.ToString();
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace MyStringApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			char[] array = new char[] { 'H', 'e', 'l', 'l', 'o', ' ', 'f', 'r', 'o', 'm', ' ', 'a', 'r', 'r', 'a', 'y' };
+			MyString emptyString = MyString.EMPTY_STRING;
+			Console.WriteLine("Empty string: {0}", emptyString);
+			MyString helloString = new MyString("Hello from string");
+			Console.WriteLine("String constructor: {0}", helloString);
+			MyString fromArray = new MyString(array);
+			Console.WriteLine("Char array constructor: {0}", fromArray);
+			MyString fromArrayPart = new MyString(array, 0, 5);
+			Console.WriteLine("Char array part constructor: {0}", fromArrayPart);
+
+			Console.WriteLine("Uppercase: {0}", helloString.ToUpper());
+			
+			Console.WriteLine("Lowercase: {0}", helloString.ToLower());
+
+			Console.WriteLine("First character by index: {0}", helloString[0]);
+
+			Console.WriteLine("MyString Length is: {0}", helloString.Lenght);
+
+			Console.WriteLine("MyString '{0}' Contains 'from' : {1}", helloString, helloString.Contains(new MyString("from")));
+
+			Console.WriteLine("MyString '{0}' Contains 'qwerty' : {1}", helloString, helloString.Contains(new MyString("qwerty")));
+
+			Console.WriteLine("MyString '{0}' Contains 'Hello' char array : {1}", 
+					helloString, helloString.Contains(new MyString("Hello").ToCharArray()));
+
+			Console.WriteLine("MyString '{0}' StartsWith 'Hello' : {1}",
+				helloString, helloString.StartsWith(new MyString("Hello")));
+
+			Console.WriteLine("MyString '{0}' EndsWith 'string' : {1}",
+				helloString, helloString.EndsWith(new MyString("string")));
+
+			Console.WriteLine("MyString '{0}' EndsWith 'string' char array: {1}",
+				helloString, helloString.EndsWith(new MyString("string").ToCharArray()));
+
+			MyString trimString = new MyString("   Hello   ");
+
+			Console.WriteLine("MyString '{0}' TrimStart: '{1}'",trimString, trimString.TrimStart());
+
+			Console.WriteLine("MyString '{0}' TrimEnd: '{1}'", trimString, trimString.TrimEnd());
+
+			Console.WriteLine("MyString '{0}' Trim: '{1}'", trimString, trimString.Trim());
+
+			MyString first = new MyString("First");
+			MyString second = new MyString("Second");
+			MyString hello = new MyString("Hello");
+			MyString anotherHello = new MyString("Hello");
+
+			Console.WriteLine("Operator + for: '{0}' and '{1}' - result: '{2}'",first, second, (first + second));
+
+			Console.WriteLine("Operator == for: '{0}' and '{1}' - result: {2}", hello, anotherHello, hello == anotherHello);
+
+			Console.WriteLine("Operator != for: '{0}' and '{1}' - result: {2}", hello, anotherHello, hello != anotherHello);
+
+			MyString abc = new MyString("abc");
+			MyString abcd = new MyString("abcd");
+
+			Console.WriteLine("Operator > for: '{0}' and '{1}' - result: {2}", abc, abcd, abc > abcd);
+
+			Console.WriteLine("Operator < for: '{0}' and '{1}' - result: {2}", abc, abcd, abc < abcd);
+		}
+	}
+}
